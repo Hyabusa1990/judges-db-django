00c04b404f689dca7ec7839c2857d5e133439212
Gero Gras <gero@gras-it.de>
2023-04-15 01:55:36 +0200

    add URLs to judge for CRUD

--- judge/urls.py
+++ judge/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from judge.views import details
 
 urlpatterns = [
-    path("<pk>/", details, name="judge.details")
+    # List and Search all Judges
+    path("", details, name="judge.list"),
+    # Create new Judge
+    path("create/", details, name="judge.create"),
+    # Show Details of Judge
+    path("<pk>/", details, name="judge.details"),
+    # Edit Details of Judge
+    path("edit/<pk>/", details, name="judge.edit"),
+    # Delete Judge
+    path("delete/<pk>/", details, name="judge.delete"),
 ]
